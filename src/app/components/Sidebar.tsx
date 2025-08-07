4663d9480ff694b1e550e2f86907c878c75ca710
Maish <bwanmaina2010@gmail.com>
2025-08-07 15:15:56 +0300

    done

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -64,7 +64,7 @@ export default function Sidebar() {
       {/* Sidebar */}
       <aside className={`
         fixed md:relative z-40
-        w-64 bg-white shadow-lg p-5 space-y-6
+        w-64 h-full fixed bg-white shadow-lg p-5 space-y-6
         transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} 
         md:translate-x-0 transition-transform duration-300 ease-in-out
         h-full
